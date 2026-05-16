1175d5fc32b838a0e768bfc0819c18d2bd5c2dac
Santiherraizz <santiherra06@gmail.com>
2026-05-16 16:02:51 +0200

    chore: setup del proyecto en Spring Boot inicial

---
+++ src/main/java/com/santiago/SimuladorDraft/SimuladorDraftApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.SimuladorDraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorDraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santiago/SimuladorDraft/SimuladorDraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.SimuladorDraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorDraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
